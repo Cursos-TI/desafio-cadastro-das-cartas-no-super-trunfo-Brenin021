786fbd063d5ea6d85ce339cb0c593d3c7cd0cef0
Brenin021 <159188587+Brenin021@users.noreply.github.com>
2025-03-24 17:29:44 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,7 +9,8 @@ int main () {
    float area1;
    float pib1;
    int turistico1;
-
+   float densidade1;
+   float percapita1;
    //Variaveis Carta 2
    char estado2;
    char codigo2[4];
@@ -18,11 +19,13 @@ int main () {
    float area2;
    float pib2;
    int turistico2;
+   float densidade2;
+   float percapita2;
 
    printf("Carta 1:\n");
 
    printf("Estado:\n");
-   scanf(" %c", &estado1); 
+   scanf(" %c", &estado1); // Adicionei um espaço antes de %c
 
    printf("Código:\n");
    scanf("%s", codigo1);
@@ -45,7 +48,7 @@ int main () {
    printf("Carta 2:\n");
 
    printf("Estado:\n");
-   scanf(" %c", &estado2); 
+   scanf(" %c", &estado2); // Adicionei um espaço antes de %c
 
    printf("Código:\n");
    scanf("%s", codigo2);
@@ -85,9 +88,25 @@ int main () {
    printf("PIB: %.2f bilhões de reais\n", pib2);
    printf("Número de Pontos Turísticos: %d\n", turistico2);
 
+   //inicio da segunda fase do jogo nível aventureiro
+   printf("A densidade populacional da Carta 1 é:\n");
+   densidade1 = populacao1 / area1;
+   printf("%.2f habitantes por km²\n", densidade1);
+
+   printf("A densidade populacional da Carta 2 é:\n");
+   densidade2 = populacao2 / area2;
+   printf("%.2f habitantes por km²\n", densidade2);
+  
+   printf("O PIB per capita da Carta 1 é:\n");
+   percapita1 = pib1 / populacao1;
+   printf("R$ %.2f por habitante\n", percapita1);
+
+   printf("O PIB per capita da Carta 2 é:\n");
+   percapita2 = pib2 / populacao2;
+   printf("R$ %.2f por habitante\n", percapita2);
+   
    return 0;
 }
- 
 
    
 
